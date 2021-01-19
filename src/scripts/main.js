089d04be12b2b78b90908ecfcdb45aba71ac9e38
gustavoferrazrocha <gustavoguusfr@gmail.com>
2021-01-18 21:22:41 -0300

    feat: adicionado animação da moeda

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,7 @@ let storeLevel = 0;
 function clickBtn(){
     score ++;
     document.getElementsByClassName('coin').innerHTML = 'Cliques:' + score;
+    coin.style.transform = 'scale(1.1)'
+    setInterval(function(){coin.style.transform = 'scale(1.0)'},  500)
 }
 
