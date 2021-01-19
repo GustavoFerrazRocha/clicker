f78e48ed6d673d0268ebe6c5b3fa1c13506e5473
gustavoferrazrocha <gustavoguusfr@gmail.com>
2021-01-19 18:00:55 -0300

    feat: logica da loja

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
+const store = document.getElementById('store');
 let score = 0;
-let storeLevel = 0; 
+let storeLevel = 1; 
+let currentScore = document.getElementsByClassName('coin')[0].innerHTML = 'Coins: ' + score;
+
 
 
 function clickBtn(){
-    score ++;
-    document.getElementsByClassName('coin')[0].innerHTML = 'Cliques: ' + score;
+    score = score + 1;
+    currentScore;
     coin.style.transform = 'scale(1.1)'
     setInterval(function(){coin.style.transform = 'scale(1.0)'},  500)
 }
 
+function storeUp(){
+   let scoreMin = storeLevel * 100;
+    if (storeLevel <= 99 && score >= scoreMin ) {
+        score = score - (100 * storeLevel);
+        storeLevel++;
+        currentScore;
+    } 
+}
