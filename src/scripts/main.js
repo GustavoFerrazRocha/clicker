efc25df22aec95037eae880606cc0f198421d467
letscodebrbot <letscodebrbot@gmail.com>
2021-01-19 16:42:57 -0300

    fixed: score innerhtml

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ let storeLevel = 0;
 
 function clickBtn(){
     score ++;
-    document.getElementsByClassName('coin').innerHTML = 'Cliques:' + score;
+    document.getElementsByClassName('coin')[0].innerHTML = 'Cliques: ' + score;
     coin.style.transform = 'scale(1.1)'
     setInterval(function(){coin.style.transform = 'scale(1.0)'},  500)
 }
